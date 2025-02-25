988a44226f4a344bde094eea0c82fe2074ab8d82
Azaan Ahmed <azaanbaba007@gmail.com>
2025-02-25 20:24:10 +0530

    Updated the solution to an optimised one using Binary Search.

--- Medium/Find-first-and-last-position-of-element-in-sorted-array/Solution.java
+++ Medium/Find-first-and-last-position-of-element-in-sorted-array/Solution.java
@@ -1,28 +1,41 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int[] index = {-1, -1};
-        int right = 0;
-        int left = nums.length - 1;
-        if (nums.length == 0){
-            return index;
-        } else {
-            while(right <= left){
-                if(nums[right] == target){
-                    index[0] = right;
-                    break;
-                }
-                right++;
-            }
-            while (left >= right){
-                if(nums[left] == target){
-                    index[1] = left;
-                    break;
-                }
-                left--;
+        int start = 0;
+        int end = nums.length - 1;
+        int pos_st = -1;
+        int pos_end = -1;
+
+        // Find first occurrence (leftmost index)
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                pos_st = mid;
+                end = mid - 1;
+            } else if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
             }
         }
 
-        return index;
+        if (pos_st == -1) return new int[]{-1, -1}; // Target not found, return [-1, -1]
 
+        // Find last occurrence (rightmost index)
+        start = pos_st;
+        end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                pos_end = mid;
+                start = mid + 1;
+            } else if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        return new int[]{pos_st, pos_end};
     }
 }
