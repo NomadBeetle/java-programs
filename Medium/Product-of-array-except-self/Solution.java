53ce75f2aa57e78d978f09aa0ad77212a206331f
Azaan Ahmed <azaanahmed1369@gmail.com>
2026-07-21 13:48:29 +0530

    Updated Solution

--- Medium/Product-of-array-except-self/Solution.java
+++ Medium/Product-of-array-except-self/Solution.java
@@ -1,22 +1,21 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int[] answer = new int[nums.length];
-        int pre = 1;
-        int suff = 1;
-        
-        // Build prefix product
-        answer[0] = 1;
-        for (int i = 0; i < nums.length - 1; i++) {
-            pre *= nums[i];
-            answer[i + 1] = pre;
+        int[] ans = new int[nums.length];
+        int leftProduct = 1;
+        int n = nums.length;
+
+        for (int i = 0; i < nums.length; i++){
+            ans[i] = leftProduct;
+            leftProduct *= nums[i];
         }
 
-        // Build suffix product and combine with prefix
-        for (int i = nums.length - 1; i > 0; i--) {
-            suff *= nums[i];
-            answer[i - 1] *= suff;
+        int rightProduct = 1;
+
+        for (int i = n - 1; i >= 0; i--){
+            ans[i] *= rightProduct;
+            rightProduct *= nums[i];
         }
 
-        return answer;
+        return ans;
     }
 }
